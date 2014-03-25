d358c06637ecc4f793c0c2a6c171102e165b7a8a
Nami-Doc <vendethiel@hotmail.fr>
2014-03-25 10:21:43 +0100

    Trying to get forms working. Might need to go back to scaml ...

--- app/controllers/Ideas.scala
+++ app/controllers/Ideas.scala
@@ -4,11 +4,18 @@ import play.api._
 import controllers.tools.Render
 import play.api.mvc._
 import models._
+import play.api.data._
+import play.api.data.Forms._
 
 object Ideas extends Controller {
   def index = Action { implicit request =>
+    val form = Form(
+      "title" -> nonEmptyText
+    )
+
     Ok(Render.withStds("ideas/index.jade",
-      'ideas -> Idea.all
+      'ideas -> Idea.all,
+      'form -> form
     ))
   }
 
